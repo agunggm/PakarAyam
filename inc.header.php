240ae89ae37fc02678cfc6aae1b9fd8e6549f7c5
Agung Gumelar <agung_gm@outlook.com>
2017-12-29 17:09:07 +0700

    Nama Kelompok

--- inc.header.php
+++ inc.header.php
@@ -5,10 +5,18 @@
 <body>
 <table width="800" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#22B5DD">
   <tr>
-    <td align="center"><b>[ HEADER ] </b></td>
+    <td align="center" colspan="2"><b>[ HEADER ] </b></td>
   </tr>
   <tr>
     <td bgcolor="#DBEAF5"><a href="http://www.bunafit-komputer.com"><img src="img/header.gif" alt="http://www.bunafit-komputer.com" width="300" height="80" border="0"></a></td>
+    <td bgcolor="#DBEAF5" align="center">
+      <pre>
+                Kelompok Sistem Pakar :
+            - Agung Gumelar       5520114073
+            - M. Reza Noviyanto   5520114056
+            - Regi Permana        5520114053
+        </pre>
+    </td>
   </tr>
 </table><br>
 </body>
